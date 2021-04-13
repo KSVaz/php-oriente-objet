e65d5a3b3b0a444725815d93b71dbb329f81a8fe
Katia Semedo <katia.semedo@10mentionweb.org>
2021-04-13 17:24:50 +0200

    mise en page

---
+++ inc/nav.inc.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark shadow">
+    <a class="navbar-brand" href="#">Cours POO</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExample04">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link active" href="blog.php">Blog</a>
+            </li>
+            <!-- <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown04">
+                    <a class="dropdown-item" href="humain/index_humain.php"></a>
+                </div>
+            </li>  -->
+        </ul>
+    </div>
+</nav>
